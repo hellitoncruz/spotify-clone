70eec97e8025bd436c4a4bb6a9027025b80ce932
Helliton <hellitoncruz16@gmail.com>
2022-02-22 21:32:01 -0300

    responsividade

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-let butao = document.querySelector('#butao');
 
-        butao.addEventListener('mouseover', onmouse);
-        butao.addEventListener('mouseout', outmouse);
 
-        function onmouse() {
-            butao.style.padding = "1.6rem 3.6rem";
-        }
-        function outmouse() {
-            butao.style.padding = "1.5rem 3.5rem";
-        }
+let menuCl = document.querySelector('#menuClose')
+let menu = document.querySelector('#menu')
+let menuContainer = document.querySelector('.menu')
+let logo = document.querySelector('#logo1')
+
+menu.addEventListener('click', menuOpen)
+menuCl.addEventListener('click', menuClose);
+
+function menuOpen() {
+    menuContainer.style.display = 'block'
+    menu.style.display ='none'
+    menuCl.style.display = 'block'
+    logo.style.display = 'none'
+}
+
+function menuClose() {
+    menuContainer.style.display = 'none'
+    menuCl.style.display = 'none'
+    menu.style.display ='block'
+    logo.style.display = 'block'
+}
